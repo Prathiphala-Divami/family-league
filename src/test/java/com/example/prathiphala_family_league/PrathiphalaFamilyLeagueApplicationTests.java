0e49f80b652d9921e5206d47c60c1d42f4fbddd2
Prathiphala <prathiphala@LAP-0020.local>
2026-05-25 11:18:38 +0530

    resolve test and startup failures

--- src/test/java/com/example/prathiphala_family_league/PrathiphalaFamilyLeagueApplicationTests.java
+++ src/test/java/com/example/prathiphala_family_league/PrathiphalaFamilyLeagueApplicationTests.java
@@ -2,9 +2,11 @@ package com.example.prathiphala_family_league;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-class PrathiphalaFamilyLeagueApplicationTests {
+@ActiveProfiles("it")
+class PrathiphalaFamilyLeagueApplicationTests extends AbstractIntegrationTest {
 
 	@Test
 	void contextLoads() {
